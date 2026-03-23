61f4c9291204eed8feb2dfaf51ba4171352cd4a0
Tasin Farhan <tasinfarhan1016@gmail.com>
2026-03-23 21:44:42 +0600

    Added origami folding

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -8,23 +8,23 @@ map("i", "jk", "<ESC>")
 map("i", "<M-Space>", require("cmp").complete, { desc = "Trigger completion" })
 map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
 
-local ts_folds_enabled = false
-
-local function toggle_treesitter_folds()
-  ts_folds_enabled = not ts_folds_enabled
-
-  if ts_folds_enabled then
-    vim.opt.foldmethod = "expr"
-    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-  else
-    vim.opt.foldmethod = "manual"
-    vim.opt.foldexpr = "0"
-    vim.cmd "normal! zE"
-  end
-end
-
-map("n", "<leader>zf", toggle_treesitter_folds, {
-  desc = "Toggle Treesitter Folds",
-})
-
+-- local ts_folds_enabled = false
+--
+-- local function toggle_treesitter_folds()
+--   ts_folds_enabled = not ts_folds_enabled
+--
+--   if ts_folds_enabled then
+--     vim.opt.foldmethod = "expr"
+--     vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+--   else
+--     vim.opt.foldmethod = "manual"
+--     vim.opt.foldexpr = "0"
+--     vim.cmd "normal! zE"
+--   end
+-- end
+--
+-- map("n", "<leader>zf", toggle_treesitter_folds, {
+--   desc = "Toggle Treesitter Folds",
+-- })
+--
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+++ .config/nvim/lua/plugins/origami.lua
@@ -0,0 +1,11 @@
+-- lazy.nvim
+return {
+  "chrisgrieser/nvim-origami",
+  event = "VeryLazy",
+  opts = {},
+
+  init = function()
+    vim.opt.foldlevel = 99
+    vim.opt.foldlevelstart = 99
+  end,
+}
